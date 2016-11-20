c737b9c2f9acba273533c271855ae4d75c2d2dac
domen1806 <domen1806@gmail.com>
2016-11-20 13:23:07 +0100

    login

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class User
+ * @package AppBundle\Entity
+ * @ORM\Entity
+ * @ORM\Table(name="`user`")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
